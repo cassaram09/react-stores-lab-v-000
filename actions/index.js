c17203fe46aa5a87e46bc81c217561b9e5aef865
cassaram09 <cassaram09@gmail.com>
2017-05-02 19:19:08 +0100

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,17 @@
 // Your implementation here.
 // Hint: We need two actions!
 
-export default {}
+import counterStore from '../stores/counterStore'
+
+function increment(){
+  counterStore.increment();
+}
+
+function decrement(){
+  counterStore.decrement();
+}
+
+export default {
+  increment,
+  decrement
+}
--- stores/counterStore.js
+++ stores/counterStore.js
@@ -1,6 +1,22 @@
-class CounterStore {
-  // Your implementation here.
-  // Hint: extend the Store class!
+import Store from '../stores/Store';
+
+class CounterStore extends Store {
+  
+  constructor() {
+    super()
+    this.state = 0;
+  }
+
+  increment(){
+    const newState = this.getState() +1;
+    this.setState(newState)
+  }
+
+  decrement () {
+    const newState = this.getState() - 1;
+    this.setState(newState);
+  }
+
 }
 
 const counterStore = new CounterStore();
